f80c412accdf27afac8696d458513ce1d7daa8b0
Picode08 <chikasienne@gmail.com>
2021-01-16 02:38:26 +0900

    create customer

--- app/controllers/customer/users_controller.rb
+++ app/controllers/customer/users_controller.rb
@@ -1,2 +1,25 @@
 class Customer::UsersController < Customer::MainController
+	
+	def index
+	end
+	
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
 end
