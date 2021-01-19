8189d764a47a6696101de5ab0bb39f2451da4d84
abineko2 <yuki1980426@gmail.com>
2021-01-20 02:06:48 +0900

    1/20 新規店頭商品処理

--- app/controllers/employee/products_controller.rb
+++ app/controllers/employee/products_controller.rb
@@ -8,7 +8,16 @@ class Employee::ProductsController < Employee::MainController
   end
 
   def create
-    @product = Product.new(product_parameters)
+    item = Item.find_by(name:  product_parameters[:name]) 
+    process = item.itemProcesses 
+    sale_date = Date.today + 1
+    @product = Product.new(
+       name:  product_parameters[:name],
+       price: product_parameters[:price],
+       process: process,
+       seles_date: sale_date,
+       stock: product_parameters[:stock]
+    )
     if @product.save
       redirect_to employee_products_path, info: "店頭商品#{@product.name}を登録しました。"
     else
@@ -22,5 +31,6 @@ private
   def product_parameters
     params.require(:product).permit( :name, :price, :stock )
   end
+  
   
 end
