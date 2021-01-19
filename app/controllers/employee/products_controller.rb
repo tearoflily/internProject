c024d48f7da4e49e02ac5117708babbb0964ca33
msk-ya <masakiabineko@gmail.com>
2021-01-19 20:03:54 +0900

    1/19 店頭商品新規ページ

--- app/controllers/employee/products_controller.rb
+++ app/controllers/employee/products_controller.rb
@@ -1,10 +1,26 @@
-class Employee::ProductsController < ApplicationController
+class Employee::ProductsController < Employee::MainController
   def index
+    @products = Product.all.datasort
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_parameters)
+    if @product.save
+      redirect_to employee_products_path, info: "店頭商品#{@product.name}を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
   end
+private
+  def product_parameters
+    params.require(:product).permit( :name, :price, :stock )
+  end
+  
 end
