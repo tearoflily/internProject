bc690cf9efce584256d8a54a5aca0c94604d8d49
Picode08 <chikasienne@gmail.com>
2021-01-20 22:56:38 +0900

    1/20ログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'top/show'
   root 'employee/items#index'
+  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 #####################植松エリア##################################################################################
   namespace :employee do
     resources :items
