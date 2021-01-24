c4f0f4ab90166714b17d37d4d5d891a150305dca
abineko2 <yuki1980426@gmail.com>
2021-01-24 19:59:00 +0900

    1/22注文ステータス変更

---
+++ spec/factories/order.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+    factory :order do
+      name { "さんま" }
+      price{ 100 }
+      num{ 3 }
+      status{ 'in_order'}
+      seles_date{ Date.today }
+      order_time{ DateTime.now }
+      user
+    end
+end
