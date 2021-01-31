cfc960f3a8b9e66fc6d0e74269906c7fbfbe36bb
Picode08 <chikasienne@gmail.com>
2021-01-31 11:33:46 +0900

    1/30ユーザー登録＆ログイン画面作成

--- app/controllers/customer/users_controller.rb
+++ app/controllers/customer/users_controller.rb
@@ -20,7 +20,7 @@ class Customer::UsersController < Customer::MainController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: '新規登録に成功しました。'
+      redirect_to top_show_path, success: '新規登録に成功しました。'
     else
       render :new
     end
