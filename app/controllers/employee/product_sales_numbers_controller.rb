52e32320faae4bb63685a2669c19a30895f3aaf4
abineko2 <yuki1980426@gmail.com>
2021-02-05 00:04:59 +0900

    2/4商品別数終了

--- app/controllers/employee/product_sales_numbers_controller.rb
+++ app/controllers/employee/product_sales_numbers_controller.rb
@@ -3,7 +3,9 @@ class Employee::ProductSalesNumbersController <  Employee::MainController
     @item = Item.find( params[:id] )
     @month = params[:month].present? ?Date.parse(params[:month]) : Date.today
     orders = Order.saleByProduct(@item.name, @month)
-    
     @graphDatas = Order.salesCountArray(orders)
+    @table_datas = Order.salesQuantityTable(orders, @month)
+    @total_numbers = Order.totalNumber(orders)
+  
   end
 end
