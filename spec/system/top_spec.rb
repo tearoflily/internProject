4014aa59940dc99bb194a9d06fa9415dc5402f41
Picode08 <chikasienne@email.com>
2021-03-03 15:56:05 +0900

    3/3TOP画面rspec追加修正

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -5,32 +5,45 @@ RSpec.describe 'Top', type: :system do
   describe "TOP画面表示" do
     let!(:product){ FactoryBot.create(:product, name: 'まだい')}
     let!(:item){ FactoryBot.create(:item, name: 'まだい')}  
-    before do
-      visit top_show_path
-    end
     it "項目が表示されていること" do
+      travel_to Time.zone.local(2021, 03, 04) do
+        visit top_show_path
         expect(page).to have_content "まだい"
         expect(page).to have_no_selector ".soldout" 
+      end
     end
   end
 
-  describe "TOP画面表示" do
-    let!(:product){ FactoryBot.create(:product, name: 'まだい', stock: '1')}
-    let!(:item){ FactoryBot.create(:item, name: 'まだい')}  
-    before do
-      visit top_show_path
-    end
-    it "soldoutの表示" do
-      expect(page).to have_selector ".soldout" 
-    end
-  end
+  # describe "TOP画面表示" do
+  #   let!(:product){ FactoryBot.create(:product, name: 'まだい', stock: '1')}
+  #   let!(:item){ FactoryBot.create(:item, name: 'まだい')}  
+  #   before do
+  #     visit top_show_path
+  #   end
+  #   it "soldoutの表示" do
+  #     travel_to Time.zone.local(2021, 03, 04) do
+  #       visit top_show_path
+  #       expect(page).to have_selector ".soldout" 
+  #     end
+  #   end
+  # end
 
   describe "TOP画面表示（何もない時）" do
+    it "メッセージが表示されていること" do
+      travel_to Time.zone.local(2021, 03, 04) do
+        visit top_show_path
+        expect(page).to have_content "本日商品はありません"
+      end
+    end
+  end
+
+  describe "TOP画面表示（時間外の時）" do
     before do
-      visit top_show_path
+      travel_to(Time.new(2021, 3, 4, 12, 0, 0))
     end
     it "メッセージが表示されていること" do
-        expect(page).to have_content "本日商品はありません"
+      visit top_show_path
+      expect(page).to have_content "ただいま準備中です。もうしばらくお待ちください。"
     end
   end
 
